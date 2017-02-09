9d520f6d92552910cabbdb045abf751d1320e07a
Krisztian Gyuris <kgyuris@cognex.com>
2017-02-09 11:17:22 +0100

    adding code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp.SQLite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com"});
+                var count = db.SaveChanges(); 
+
+                Console.WriteLine($"{count} records saved to database.");
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database");
+                foreach(var blog in db.Blogs)
+                {
+                    Console.WriteLine($" - {blog.Url}");
+                }
+            }
+        }
+    }
+}
